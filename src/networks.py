ea27045a6aa87cbc20fb5c15f08c8b770ad545ea
Aayush Gogia <aayushar@yahoo.com>
2018-03-30 17:43:34 +0530

    Finished init

--- src/networks.py
+++ src/networks.py
@@ -2,18 +2,19 @@ import numpy as np
 
 
 class Network:
-    def __init__(self, layers_size):
+    def __init__(self, layers):
         self.sizes = layers_size
-        self.weights = np.random.randn()
-        self.bias = np.random.randn()
+        self.layers_size = len(layers)
+        self.weights = [np.random.randn(r,c) for r,c in zip(layers[:-1], layers[1:])]
+        self.bias = [np.random.randn(k,1) for k in sizes]
 
-    def forward(self):
-        self.sigmoid()
-
-    def MiniBatch(self):
+    def forward(self, input):
         #TODO
 
     def SGD(self):
+        #TODO
+
+    def MiniBatch(self):
         #TODO
 
     def backprop(self):
@@ -23,4 +24,4 @@ class Network:
         #TODO
 
     def del_sigmoid(self):
-        #TODO            
+        #TODO
